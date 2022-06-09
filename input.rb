c55cddb7bec890eccc0c4092f4cb543700f32b9c
Ludmila Meireles <ludmilabmeireles@gmail.com>
2022-06-09 09:07:38 -0300

    Add input section

---
+++ input.rb
@@ -0,0 +1,16 @@
+###Input de dados###
+
+#Como capturar um dado de entrada?
+#Utilizamos o metodo gets() ou gets
+#Este metodo possibilita fazer um prompt para a entrada de dados, e assim
+# que digitar o valor e clicar em enter, esse valor vai ser salvo em
+# uma variavel
+#nome = gets() #o ruby abre o prompt e espera o usuario entrar com um valor
+# para salvar na variavel nome
+#p nome #imprime o nome salvo na variavel
+#p nome.chomp() #retorna a string sem o ultimo caractere especial
+#nome = gets.chomp #já executa o chomp logo depois de capturar o dado
+
+p "Digite seu nome: "
+nome1 = gets.chomp
+#puts "Seu nome é #{nome1} "
