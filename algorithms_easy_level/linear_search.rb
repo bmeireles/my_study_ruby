bedaffea378c84c73f4d0a5e469bb3c6020f779c
Ludmila Meireles <ludmilabmeireles@gmail.com>
2022-06-14 18:08:28 -0300

    feat: linear search started

--- algorithms_easy_level/linear_search.rb
+++ algorithms_easy_level/linear_search.rb
@@ -1,1 +1,24 @@
 ###Linear search###
+
+p "Digite 5 numeros para a lista. Digite um de cada vez."
+numero1 = gets.chomp.to_i
+numero2 = gets.chomp.to_i
+numero3 = gets.chomp.to_i
+numero4 = gets.chomp.to_i
+numero5 = gets.chomp.to_i
+lista = Array.new
+lista << numero1 << numero2 << numero3 << numero4 << numero5 
+#lista = [1,23,14,24,67,54,81,17,40,58,10]
+p "Agora digite o numero que deseja encontrar na lista: "
+numero = gets.chomp.to_i
+#numero = 10
+#i = 0
+for i in lista
+    if numero == lista[i]
+    puts "O numero #{numero} está presente no index #{i}"
+    end
+end
+#puts "O numero #{numero} nao se encontra na lista."
+
+
+
--- learning_ruby_course/strings.rb
+++ learning_ruby_course/strings.rb
@@ -1,7 +1,7 @@
 #Strings
 name = "Ludmila"
-message = %q(welcome to my program, #(name)!)
+message = %q(welcome to my program, #{name}!)
 puts message
 
 puts message.length
-puts "the size of your message is: #(message.length)"
+puts "the size of your message is: #{message.length}"
